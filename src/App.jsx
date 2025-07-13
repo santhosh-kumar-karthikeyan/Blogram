2be45afc5447ec724dcdd96d78d9451c6c2d4b24
Santhosh <santhoshkumarkarthikeyan5@gmail.com>
2025-07-13 13:22:27 +0530

    add minimal dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Navbar } from "./components"
+import { Navbar } from "./components";
 
 export default function App() {
+  document.documentElement.classList.add("dark:bg-stone-800");
   return (
     <>
-      <Navbar/>
+      <Navbar />
     </>
-  )
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,31 @@
-import { useEffect } from "react";
+import { LuSunDim } from "react-icons/lu";
+import { AiOutlineMoon } from "react-icons/ai";
+import { useState, useEffect } from "react";
+import darkLogo from "/assets/Blogram3/vector/isolated-monochrome-black.svg";
+import lightLogo from "/assets/Blogram3/vector/isolated-monochrome-white.svg";
 
 export default function Navbar() {
-  useEffect(() => {
-    const variable1 = "nigga";
-    const variable2 = false;
-    console.table({ variable1, variable2 });
-  }, []);
+  const [isDark, setDark] = useState(true);
+  useEffect(() => setDark(true), []);
+  function changeTheme() {
+    setDark((currTheme) => !currTheme);
+    document.documentElement.classList.toggle("dark");
+  }
   return (
-    <nav className="w-screen bg-white h-[80px] z-10 fixed drop-shadow-lg flex justify-between items-center px-10">
-      <img
-        src="/assets/Blogram3/vector/isolated-monochrome-black.svg"
-        alt=""
-        width={"300px"}
-      />
-      <ul className="flex justify-evenly space-x-6">
+    <nav className="dark:bg-stone-800 dark:text-stone-100 font-medium w-screen bg-white h-[80px] z-10 fixed drop-shadow-lg flex justify-between items-center px-10">
+      <img src={isDark ? darkLogo : lightLogo} alt="Logo" width={"200px"} />
+      <ul className="flex justify-evenly items-center space-x-6">
         <li>Home</li>
         <li>About</li>
-        <li></li>
+        <button
+          className="hover:bg-zinc-500/10 p-1 rounded-sm"
+          onClick={changeTheme}>
+          {isDark ? (
+            <LuSunDim size={"25px"} />
+          ) : (
+            <AiOutlineMoon size={"25px"} />
+          )}
+        </button>
       </ul>
     </nav>
   );
